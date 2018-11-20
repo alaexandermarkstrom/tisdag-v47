45fc0bc88d398f91a8edee4f541fed0c0f81aa27
Alexander Markström <maal16@nti.umea>
2018-11-20 11:18:10 +0100

    test2

--- testare.py
+++ testare.py
@@ -1,6 +1,22 @@
-hej = 1
-hej = ord("a")
-hej = chr(10)
+#hej = 1
+#hej = ord("a")
+#hej = chr(10)
 
-print("värde")
-print("hej")
+#print("värde")
+#print("hej")
+
+options = 0
+
+while options != 3:
+    try:
+        options = int(input("välj ett nummer mellan 1-3\n:"))
+    except:
+        print("måste vara ett nummer")
+    if options == 1:
+        print("du har valt 1")
+    elif options == 2:
+        print("du har valt 2")
+    elif options == 3:
+        break
+    else:
+        print("kan du inte nummer eller")
